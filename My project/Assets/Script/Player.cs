ca9b2c611816a71b2a9e68895375c94005d34bbe
manaqua138 <manaqua1231@gmail.com>
2025-04-04 14:08:56 +0900

    20250404_study1

---
+++ My project/Assets/Script/Player.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+
+public class Player : Entity
+{
+
+    [Header("이동 정보")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("대쉬 정보")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+    private float dashTime;
+
+    [SerializeField] private float dashCooldown;
+    private float dashCooldownTimer;
+
+    [Header("공격 정보")]
+
+    [SerializeField] private float comboTime = 0.3f;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int comboCounter;
+
+    private float xInput;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        CheckInput();
+        Movement();
+
+        dashTime -= Time.deltaTime;
+        dashCooldownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
+
+        FlipController();
+        AnimatorControllers();
+
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+
+
+        comboCounter++;
+
+
+        if (comboCounter > 2)
+            comboCounter = 0;
+
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            DashAbility();
+        }
+
+    }
+
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+            return;
+
+        if (comboTimeWindow < 0)
+            comboCounter = 0;
+
+        isAttacking = true;
+        comboTimeWindow = comboTime;
+    }
+
+    private void DashAbility()
+    {
+        if (dashCooldownTimer < 0 && !isAttacking)
+        {
+            dashCooldownTimer = dashCooldown;
+            dashTime = dashDuration;
+        }
+    }
+
+    private void Movement()
+    {
+
+        if (isAttacking)
+        {
+            rb.linearVelocity = new Vector2(0, 0);
+        }
+        else if (dashTime > 0)
+        {
+            rb.linearVelocity = new Vector2(facingDir * dashSpeed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    //Alt + 화살표키
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.linearVelocity.x != 0;
+
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDashing", dashTime > 0);
+        anim.SetBool("isAttacking", isAttacking);
+        anim.SetInteger("comboCounter", comboCounter);
+
+    }
+
+    private void FlipController()
+    {
+        if (rb.linearVelocityX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.linearVelocityX < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+
+}
---
+++ My project/Assets/Script/PlayerAnimEvents.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private Player player;
+
+    void Start()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    public void AnimationTrigger()
+    {
+        player.AttackOver();
+    }
+
+
+}
